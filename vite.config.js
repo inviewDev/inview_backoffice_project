1215ed81eae2212ae88f7e4e07c04eb75b472e0f
I&VIEW_Development <inviewcs@gmail.com>
2025-09-02 09:44:52 +0900

    cchee
    modify

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3000',
         changeOrigin: true,
-        secure: false,
-        // rewrite 제거
-        // rewrite: (path) => path.replace(/^\/api/, '')
+        secure: false
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 });
